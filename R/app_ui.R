e953ab2d1ff8cfc41e38e49dc57b133bcdb611a3
thomasyu888 <thomas.yu@sagebase.org>
2021-07-20 18:15:34 +0800

    Comment out code

--- R/app_ui.R
+++ R/app_ui.R
@@ -27,9 +27,9 @@ app_ui <- function(request) {
 #' @noRd
 golem_add_external_resources <- function(){
 
-  add_resource_path(
-    'www', app_sys('app/www')
-  )
+  # add_resource_path(
+  #   'www', app_sys('app/www')
+  # )
 
   tags$head(
     favicon(),
